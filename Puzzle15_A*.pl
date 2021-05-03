bae00b835f633468c9a042d70bf5b7af7e18e7dc
Ronald Albert <ronaldalbert1609@gmail.com>
2021-05-03 16:36:42 -0300

    Update Puzzle15_A*.pl

--- Puzzle15_A*.pl
+++ Puzzle15_A*.pl
@@ -87,3 +87,91 @@ acao([[B, C, D, -1], [E, F, G, A], [H, I, J, K], [L, M, N, O]],
 acao([[B, C, D, E], [F, G, H, A], [I, J, K, -1], [L, M, N, O]], 
      baixo24, 
      [[B, C, D, E], [F, G, H, -1], [I, J, K, A], [L, M, N, O]]).
+
+%Movimentos possíveis para a nona casa.
+acao([[B, C, D, E], [F, G, H, I], [A, -1, J, K], [L, M, N, O]], 
+     direita31, 
+     [[B, C, D, E], [F, G, H, I], [-1, A, J, K], [L, M, N, O]]).
+acao([[B, C, D, E], [-1, F, G, H], [A, I, J, K], [L, M, N, O]], 
+     cima31, 
+     [[B, C, D, E], [A, F, G, H], [-1, I, J, K], [L, M, N, O]]).
+acao([[B, C, D, E], [F, G, H, I], [A, J, K, L], [-1, M, N, O]], 
+     baixo31, 
+     [[B, C, D, E], [F, G, H, I], [-1, J, K, L], [A, M, N, O]]).
+
+%Movimentos possíveis para a decima casa.
+acao([[B, C, D, E], [F, G, H, I], [-1, A, J, K], [L, M, N, O]], 
+     esquerda32, 
+     [[B, C, D, E], [F, G, H, I], [A, -1, J, K], [L, M, N, O]]).
+acao([[B, C, D, E], [F, G, H, I], [J, A, -1, K], [L, M, N, O]], 
+     direita32, 
+     [[B, C, D, E], [F, G, H, I], [J, -1, A, K], [L, M, N, O]]).
+acao([[B, C, D, E], [F, -1, G, H], [I, A, J, K], [L, M, N, O]], 
+     cima32, 
+     [[B, C, D, E], [F, A, G, H], [I, -1, J, K], [L, M, N, O]]).
+acao([[B, C, D, E], [F, G, H, I], [J, A, K, L], [M, -1, N, O]], 
+     baixo32, 
+     [[B, C, D, E], [F, G, H, I], [J, -1, K, L], [M, A, N, O]]).
+
+%Movimentos possíveis para a decima primeira casa.
+acao([[B, C, D, E], [F, G, H, I], [J, -1, A, K], [L, M, N, O]], 
+     esquerda33, 
+     [[B, C, D, E], [F, G, H, I], [J, A, -1, K], [L, M, N, O]]).
+acao([[B, C, D, E], [F, G, H, I], [J, K, A, -1], [L, M, N, O]], 
+     direita33, 
+     [[B, C, D, E], [F, G, H, I], [J, K, -1, A], [L, M, N, O]]).
+acao([[B, C, D, E], [F, G, -1, H], [I, J, A, K], [L, M, N, O]], 
+     cima33, 
+     [[B, C, D, E], [F, G, A, H], [I, J, -1, K], [L, M, N, O]]).
+acao([[B, C, D, E], [F, G, H, I], [J, K, A, L], [M, N, -1, O]], 
+     baixo33, 
+     [[B, C, D, E], [F, G, H, I], [J, K, -1, L], [M, N, A, O]]).
+
+%Movimentos possíveis para a decima segunda casa.
+acao([[B, C, D, E], [F, G, H, I], [J, K, -1, A], [L, M, N, O]], 
+     esquerda34, 
+     [[B, C, D, E], [F, G, H, I], [J, K, A, -1], [L, M, N, O]]).
+acao([[B, C, D, E], [F, G, H, -1], [I, J, K, A], [L, M, N, O]], 
+     cima34, 
+     [[B, C, D, E], [F, G, H, A], [I, J, -K, -1], [L, M, N, O]]).
+acao([[B, C, D, E], [F, G, H, I], [J, K, L, A], [M, N, O, -1]], 
+     baixo34, 
+     [[B, C, D, E], [F, G, H, I], [J, K, L, -1], [M, N, O, A]]).
+
+%Movimentos possíveis para a decima terceira casa.
+acao([[B, C, D, E], [F, G, H, I], [J, K, L, M], [A, -1, N, O]], 
+     direita41, 
+     [[B, C, D, E], [F, G, H, I], [J, K, L, M], [-1, A, N, O]]).
+acao([[B, C, D, E], [F, G, H, I], [-1, J, K, L], [A, M, N, O]], 
+     cima41, 
+     [[B, C, D, E], [F, G, H, I], [A, J, K, L], [-1, M, N, O]]).
+
+%Movimentos possíveis para a decima quarta casa.
+acao([[B, C, D, E], [F, G, H, I], [J, K, L, M], [-1, A, N, O]], 
+     esquerda42, 
+     [[B, C, D, E], [F, G, H, I], [J, K, L, M], [A, -1, N, O]]).
+acao([[B, C, D, E], [F, G, H, I], [J, K, L, M], [N, A, -1, O]], 
+     direita42, 
+     [[B, C, D, E], [F, G, H, I], [J, K, L, M], [N, -1, A, O]]).
+acao([[B, C, D, E], [F, G, H, I], [J, -1, K, L], [M, A, N, O]], 
+     cima42, 
+     [[B, C, D, E], [F, G, H, I], [J, A, K, L], [M, -1, N, O]]).
+
+%Movimentos possíveis para a decima quinta casa.
+acao([[B, C, D, E], [F, G, H, I], [J, K, L, M], [N, -1, A, O]], 
+     esquerda43, 
+     [[B, C, D, E], [F, G, H, I], [J, K, L, M], [N, A, -1, O]]).
+acao([[B, C, D, E], [F, G, H, I], [J, K, L, M], [N, O, A, -1]], 
+     direita43, 
+     [[B, C, D, E], [F, G, H, I], [J, K, L, M], [N, O, -1, A]]).
+acao([[B, C, D, E], [F, G, H, I], [J, K, -1, L], [M, N, A, O]], 
+     cima43, 
+     [[B, C, D, E], [F, G, H, I], [J, K, A, L], [M, N, -1, O]]).
+
+%Movimentos possíveis para a decima sexta casa.
+acao([[B, C, D, E], [F, G, H, I], [J, K, L, M], [N, O, -1, A]], 
+     esquerda44, 
+     [[B, C, D, E], [F, G, H, I], [J, K, L, M], [N, O, A, -1]]).
+acao([[B, C, D, E], [F, G, H, I], [J, K, L, -1], [M, N, O, A]], 
+     cima44, 
+     [[B, C, D, E], [F, G, H, I], [J, K, L, A], [M, N, O, -1]]).
