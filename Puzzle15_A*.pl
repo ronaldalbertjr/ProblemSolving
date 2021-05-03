33fe2762856d2ba8e43318e9494c059fd0bfe3c6
Ronald Albert <ronaldalbert1609@gmail.com>
2021-05-03 19:41:51 -0300

    Update Puzzle15_A*.pl

--- Puzzle15_A*.pl
+++ Puzzle15_A*.pl
@@ -175,3 +175,76 @@ acao([[B, C, D, E], [F, G, H, I], [J, K, L, M], [N, O, -1, A]],
 acao([[B, C, D, E], [F, G, H, I], [J, K, L, -1], [M, N, O, A]], 
      cima44, 
      [[B, C, D, E], [F, G, H, I], [J, K, L, A], [M, N, O, -1]]).
+
+final_pos(1, (1,1)).
+final_pos(2, (1,2)).
+final_pos(3, (1,3)).
+final_pos(4, (1,4)).
+final_pos(5, (2,1)).
+final_pos(6, (2,2)).
+final_pos(7, (2,3)).
+final_pos(8, (2,4)).
+final_pos(9, (3,1)).
+final_pos(10, (3,2)).
+final_pos(11, (3,3)).
+final_pos(12, (3,4)).
+final_pos(13, (4,1)).
+final_pos(14, (4,2)).
+final_pos(15, (4,3)).
+final_pos(-1, (4,4)).    
+
+dist_man((X,Y), A, Dist) :-
+    final_pos(A, (Xf,Yf)),
+    abs(X-Xf, Dist_x),
+    abs(Y-Yf, Dist_y),
+    Dist is Dist_x + Dist_y.
+
+h_man([[A, B, C, D], [E, F, G, H], [I, J, K, L], [M, N, O, P]], Res) :-
+    dist_man((1,1),A,Dist_A),
+    dist_man((1,2),B,Dist_B),
+    dist_man((1,3),C,Dist_C),
+    dist_man((1,4),D,Dist_D),
+    dist_man((2,1),E,Dist_E),
+    dist_man((2,2),F,Dist_F),
+    dist_man((2,3),G,Dist_G),
+    dist_man((2,4),H,Dist_H),
+    dist_man((3,1),I,Dist_I),
+    dist_man((3,2),J,Dist_J),
+    dist_man((3,3),K,Dist_K),
+    dist_man((3,4),L,Dist_L),
+    dist_man((4,1),M,Dist_M),
+    dist_man((4,2),N,Dist_N),
+    dist_man((4,3),O,Dist_O),
+    dist_man((4,4),P,Dist_P),
+    Res is Dist_A + Dist_B + Dist_C + Dist_D + Dist_E + Dist_F + Dist_G + Dist_H + Dist_I + Dist_J + Dist_K + Dist_L + Dist_M + Dist_N + Dist_O + Dist_P.
+    
+vizinho(N, FilhosN) :- 
+    findall(Y,acao(N, _, Y),FilhosN).
+
+diffLists([], _, []) :- !.
+diffLists([H1 | T1], RL, [H1 | T]) :- not(member(H1, RL)), !, diffLists(T1, RL, T).
+diffLists([H1 | T1], RL, L) :- member(H1, RL), !, diffLists(T1, RL, L).
+
+
+
+adicionar_a_fronteira(V, F1, F3) :-
+    append(F1, V, F2),
+    sort_na_heuristica(F2, F3).
+
+buscar_em_largura([Nodo | _], _, _) :- 
+    objetivo(Nodo),
+    write(Nodo).
+buscar_em_largura([Nodo | F1], Visitados, Estados) :- 
+    %write(Nodo),
+    %nl,
+    append([Nodo], Visitados, Visitados2),
+    append([Nodo], Estados, Estados2),
+    vizinho(Nodo, V),
+    diffLists(V, Visitados2, V2),
+    adicionar_a_fronteira(V2, F1, F2),
+    append(F2, Visitados2, Visitados3),
+    buscar_em_largura(F2, Visitados3, Estados2), !.
+
+busca_em_largura(Nodo) :- buscar_em_largura([Nodo | _], [], []).
+    
+    
